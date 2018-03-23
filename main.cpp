079b99b71a3a9fc5e858f5e4066941d853399954
gscsdlz <lz842063523@foxmail.com>
2018-03-23 20:01:21 +0800

    新增相似度对比

--- main.cpp
+++ main.cpp
@@ -72,12 +72,76 @@ int compare_file()
         if(u != s)
             return -1;
     }
-
+    sin.close();
+    uin.close();
     if(emptyFileSig)
         return -1;
     else
         return 0;
 }
+
+void read_and_handle(char *path, string &cpp_str)
+{
+    ifstream in;
+    in.open(path);
+    bool multiComment = false;
+
+    while(!in.eof()) {
+        string str, tmp;
+        getline(in, tmp);
+        for(int i = 0; i < tmp.size(); i++) {
+            if(tmp[i] == '*' && i < tmp.size() - 1 && tmp[i + 1] == '/') {
+                multiComment = false;
+                i++;
+            }
+            if(!multiComment && tmp[i] == '/' && i < tmp.size() - 1 && tmp[i + 1] == '/') {  //单行注释
+                break;
+            }
+            if(tmp[i] == '/' && i < tmp.size() - 1 && tmp[i + 1] == '*') {
+                multiComment = true;
+                i++;
+            }
+            if(!multiComment) {
+                if(tmp[i] >= 33 && tmp[i] <= 126) { //33 => ! 126 => ~
+                    str.push_back(tmp[i]);
+                }
+            }
+        }
+        cpp_str += str;
+    }
+    in.close();
+}
+
+
+int compare_code()
+{
+    string ucpp, scpp;
+    read_and_handle("./user.cpp", ucpp);
+    read_and_handle("./std.cpp", scpp);
+
+    int **dp = new int *[ucpp.size() + 1];
+
+    for(int i = 0; i <= ucpp.size(); i++)
+        dp[i] = new int [scpp.size() + 1];
+
+    for(int i = 0; i <= ucpp.size(); i++)
+        for(int j = 0; j <= scpp.size(); j++)
+            dp[i][j] = 0;
+
+
+    for(int i = 1; i <= scpp.size(); i++)
+        dp[0][i] = dp[0][i - 1] + 1;
+    for(int i = 1; i <= ucpp.size(); i++)
+        dp[i][0] = dp[i - 1][0] + 1;
+    for(int i = 1; i <= ucpp.size(); i++)
+        for(int j = 1; j <= scpp.size(); j++) {
+            int tmp = ucpp[ i - 1] == scpp[j - 1] ? 0 : 1;
+            dp[i][j] = min( min(dp[i-1][j] + 1, dp[i][j-1] + 1), dp[i-1][j-1] + tmp );
+        }
+    int edit = dp[ucpp.size()][scpp.size()];
+    return 100 - edit * 100 / max(ucpp.size(), scpp.size());
+}
+
 
 int main()
 {
@@ -89,8 +153,11 @@ int main()
             cout << "begin to compare file" << endl;
             if(compare_file() == 0)
                 cout << "answer is ok" << endl;
-            else
+            else {
                 cout << "wrong answer" << endl;
+                cout << "begin to compare code" << endl;
+                cout << compare_code() << endl;
+            }
 
         }
     } else {
